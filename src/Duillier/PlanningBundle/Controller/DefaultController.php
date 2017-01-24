425470d317c71252e3b61f8a7afab1efd7d6d1c5
perrinyo <perrinyo.info@gmail.com>
2017-01-24 14:55:51 +0100

    Insertion du planning

--- src/Duillier/PlanningBundle/Controller/DefaultController.php
+++ src/Duillier/PlanningBundle/Controller/DefaultController.php
@@ -4,10 +4,11 @@ namespace Duillier\PlanningBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DuillierPlanningBundle:Default:index.html.twig');
+        return $this->render('DuillierPlanningBundle:Default:index.html.php');
     }
 }
---
+++ src/Duillier/PlanningBundle/Resources/views/Default/index.html.php
@@ -0,0 +1,56 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=Library;charset=utf8', 'root', 'root');
+$bdd->exec('INSERT INTO Book(isbn, title, price) VALUES(1234567898,\'TestSymfony\',42)');
+echo "test";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  </head>
+  <body>
+    <h1 class="text-center">Vendredi 31 mars 2017</h1>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+         <th></th>
+         <?php
+            for($poste=1; $poste<=6; $poste++){
+              echo '<th>Poste '.$poste.'</th>';
+            }
+          ?>
+       </tr>
+    </thead>
+      <tbody>
+        <?php
+          for($heure=8; $heure<=12; $heure++){
+            for($minute=0; $minute<=45; $minute +=15){
+              echo '<tr>';
+              if($heure<10){
+                if($minute<15){
+                  echo '<th>0'.$heure.'h'.$minute.'0</th>';
+                }else{
+                  echo '<th>0'.$heure.'h'.$minute.'</th>';
+                }
+              }else{
+                if($minute<15){
+                  echo '<th>'.$heure.'h'.$minute.'0</th>';
+                }else{
+                  echo '<th>'.$heure.'h'.$minute.'</th>';
+                }
+              }
+
+              for($i=0;$i<6;$i++){
+                echo '<td><a href="" class="btn btn-default">S\'inscrire</a></td>';
+              }
+              echo '</tr>';
+            }
+          }
+         ?>
+      </tbody>
+    </table>
+  </body>
+</html>
