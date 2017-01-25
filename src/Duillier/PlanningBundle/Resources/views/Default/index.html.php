ffad4ec24577c2e9612dc9b3a9b859694fb29106
perrinyo <perrinyo.info@gmail.com>
2017-01-25 14:28:36 +0100

    Planning

--- src/Duillier/PlanningBundle/Resources/views/Default/index.html.php
+++ src/Duillier/PlanningBundle/Resources/views/Default/index.html.php
@@ -1,6 +1,6 @@
 <?php
-$bdd = new PDO('mysql:host=\'https://login-16.hoststar.ch/phpMyAdmin/\';dbname=tsduillier;charset=utf8', 'tsduillier', 'Perce-neige-2017');
-//$reponse = $bdd->query('INSERT INTO Poste(idPoste) VALUES(\'1\')');
+$bdd = new PDO('mysql:host=localhost;dbname=tsduillier;charset=utf8', 'root', 'root');
+
 ?>
 
 <!DOCTYPE html>
@@ -11,43 +11,78 @@ $bdd = new PDO('mysql:host=\'https://login-16.hoststar.ch/phpMyAdmin/\';dbname=t
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
   </head>
   <body>
-    <h1 class="text-center">Vendredi 31 mars 2017</h1>
+
+    <form action="" method="post">
+      <label for="">Jour de tir : </label>
+      <select name="jour">    
+      <?php
+        $reponse = $bdd->query('SELECT * FROM jour');
+
+        while ($donnees = $reponse->fetch()){
+          //Conversion de la date en français
+          $jour_semaine = array(1=>"Lundi", 2=>"Mardi", 3=>"Mercredi", 4=>"Jeudi", 5=>"Vendredi", 6=>"Samedi", 7=>"Dimanche");
+          $nom_mois = array(1=>"Janvier", 2=>"Février", 3=>"Mars", 4=>"Avril", 5=>"Mai", 6=>"Juin", 7=>"Juillet", 8=>"Aout", 9=>"Septembre", 10=>"Octobre", 11=>"Novembre", 12=>"Décembre");
+          list($annee, $mois, $jour) = explode ("-", $donnees['date']);
+          $timestamp = mktime(0,0,0, date($mois), date($jour), date($annee));
+          $njour = date("N",$timestamp);
+          $nmois = date("n",$timestamp);
+          ?>
+              <option value="<?php echo $donnees['date']; ?>" <?php if(isset($_POST['jour'])&&$_POST['jour'] == $donnees['date']) echo "selected='selected'"; ?>>
+              <?php echo $jour_semaine[$njour]." ".$jour." ".$nom_mois[$nmois]." ".$annee; ?></option>
+          <?php
+        }
+      ?>
+      </select>
+      <input type="submit" value="Go !" class="btn btn-success">
+    </form>
+
+
+    <?php
+    $jour="2017-03-31";
+    if(isset($_POST['jour'])){
+      $jour=$_POST['jour'];
+    }
+      $reponse = $bdd->query('SELECT date FROM jour WHERE date=\''.$jour.'\'');
+      while ($donnees = $reponse->fetch()){
+        //Conversion de la date en français
+        $jour_semaine = array(1=>"Lundi", 2=>"Mardi", 3=>"Mercredi", 4=>"Jeudi", 5=>"Vendredi", 6=>"Samedi", 7=>"Dimanche");
+        $nom_mois = array(1=>"Janvier", 2=>"Février", 3=>"Mars", 4=>"Avril", 5=>"Mai", 6=>"Juin", 7=>"Juillet", 8=>"Aout", 9=>"Septembre", 10=>"Octobre", 11=>"Novembre", 12=>"Décembre");
+        list($annee, $mois, $jourN) = explode ("-", $donnees['date']);
+        $timestamp = mktime(0,0,0, date($mois), date($jourN), date($annee));
+        $njour = date("N",$timestamp);
+        $nmois = date("n",$timestamp);
+        //Affichage du titre
+        echo '<h1 class="text-center">'.$jour_semaine[$njour]." ".$jourN." ".$nom_mois[$nmois]." ".$annee.'</h1>';
+      }
+    ?>
+    <!--Affichage du tableau -->
     <table class="table table-bordered">
       <thead>
         <tr>
          <th></th>
          <?php
-            for($poste=1; $poste<=6; $poste++){
-              echo '<th>Poste '.$poste.'</th>';
-            }
+         //la ligne des postes
+         $reponse = $bdd->query('SELECT idPoste FROM poste');
+          while ($donnees = $reponse->fetch()){
+            echo '<th>Poste '.$donnees['idPoste'].'</th>';
+          }
           ?>
        </tr>
     </thead>
       <tbody>
         <?php
-          for($heure=8; $heure<=12; $heure++){
-            for($minute=0; $minute<=45; $minute +=15){
-              echo '<tr>';
-              if($heure<10){
-                if($minute<15){
-                  echo '<th>0'.$heure.'h'.$minute.'0</th>';
-                }else{
-                  echo '<th>0'.$heure.'h'.$minute.'</th>';
-                }
-              }else{
-                if($minute<15){
-                  echo '<th>'.$heure.'h'.$minute.'0</th>';
-                }else{
-                  echo '<th>'.$heure.'h'.$minute.'</th>';
-                }
-              }
-
-              for($i=0;$i<6;$i++){
-                echo '<td><a href="" class="btn btn-default">S\'inscrire</a></td>';
-              }
-              echo '</tr>';
-            }
+        //la colonne des horaires
+        $reponse = $bdd->query('SELECT heure FROM Horaire WHERE date=\''.$jour.'\'');
+        while ($donnees = $reponse->fetch()){
+          echo '<tr>';
+          echo '<th>'.$donnees['heure'].'</th>';
+          //les boutons s'inscrire dans chaque colonne
+          for($i=0;$i<6;$i++){
+            echo '<td><a href="" class="btn btn-default">S\'inscrire</a></td>';
           }
+          echo '</tr>';
+        }
+
          ?>
       </tbody>
     </table>
