754f3c1acc6cee35de170b61fc530488b2d32841
perrinyo <perrinyo.info@gmail.com>
2017-02-23 20:36:07 +0100

    SignInBundle formulaire.html.php

--- src/Duillier/SignInBundle/Resources/views/Inscription/formulaire.html.php
+++ src/Duillier/SignInBundle/Resources/views/Inscription/formulaire.html.php
@@ -1,5 +1,5 @@
 <?php
-  $bdd = new PDO('mysql:host=localhost;dbname=tsduillier;charset=utf8', 'root', '');
+$bdd = new PDO('mysql:host=localhost;dbname=tsduillier;charset=utf8', 'root', 'root');
 ?>
 
 <!DOCTYPE html>
@@ -18,88 +18,75 @@
 <body>
   <section>
 
-    <div class="col-sm-12 divFormulaire " >
-      <h1 class="page-header text-center ">Formulaire d'inscription</h1>
-    </div>
-      <div class="col-sm-5 divFormulaire " >
+
+      <h1 class="page-header text-center">Formulaire d'inscription</h1>
 
       <form class="form-horizontal" action="../planning" method="post" onsubmit="return verifForm(this)">
+      <div class="col-sm-5 divFormulaire ">
 
       <fieldset class="coordonnees">
         <legend>Vos coordonnées</legend>
         <div class="form-group">
           <label  class="col-sm-4 control-label"for="nom">Nom</label>
-
-          <div class="col-sm-7">
-            <input type="text" name="nom" class=" form-control " onblur="verifNom(this)" value="" required>
-
+          <div class="col-sm-5">
+            <input type="text" name="nom" class="form-control" value="" onblur="verifNom(this)" required>
           </div>
         </div>
 
         <div class="form-group">
           <label   class="col-sm-4 control-label" for="prenom">Prénom</label>
-          <div class="col-sm-7">
+          <div class="col-sm-5">
             <input type="text" name="prenom"  class="form-control" value="" onblur="verifNom(this)" required>
           </div>
         </div>
 
         <div class="form-group">
           <label  class="col-sm-4 control-label" for="idLicence">Numéro de licence</label>
-          <div class="col-sm-7">
-            <input type="text" name="idLicence"  class="form-control" onblur="verifLicence(this)" value="" required>
+          <div class="col-sm-5">
+            <input type="text" name="idLicence"  class="form-control" value="" onblur="verifLicence(this)" required>
           </div>
         </div>
 
         <div class="form-group">
           <label  class="col-sm-4 control-label" for="adresse">Adresse</label>
-          <div class="col-sm-7">
-            <input type="text" name="prenom"  class="form-control" value="" onblur="verifAdresse(this)" required>
+          <div class="col-sm-5">
+            <input type="text" name="adresse"  class="form-control" value="" onblur="verifAdresse(this)" required>
           </div>
         </div>
         <div class="form-group">
           <label  class="col-sm-4 control-label" for="ville">Ville</label>
-          <div class="col-sm-7">
+          <div class="col-sm-5">
             <input type="text" name="ville"  class="form-control" value="" onblur="verifVille(this)" required>
-
           </div>
         </div>
 
         <div class="form-group">
           <label  class="col-sm-4 control-label" for="codePostal">Code postal</label>
-
-          <div class="col-sm-7">
-            <input type="text" name="codePostal"  class="form-control" value="" onblur="verifCP(this)" required>
-
+          <div class="col-sm-5">
+            <input type="text" name="codePostal"  class="form-control" value="" onblur="verifCP(this)"required>
           </div>
         </div>
 
         <div class="form-group">
           <label  class="col-sm-4 control-label" for="email">E-mail</label>
-
-          <div class="col-sm-7">
+          <div class="col-sm-5">
             <input type="text" name="email"  class="form-control" value="" onblur="verifMail(this)" required>
-
           </div>
         </div>
 
         <div class="form-group">
-
-          <label  class="col-sm-4 control-label" for="anneeNaissance">Année de naissance</label>
-          <div class="col-sm-7">
-            <input type="text" name="anneeNaissance"  class="form-control" value="" placeholder="JJ/MM/AAAA" onblur="verifNaissance(this)"required>
-
+          <label  class="col-sm-4 control-label" for="dateNaissance">Date de naissance</label>
+          <div class="col-sm-5">
+            <input type="text" name="dateNaissance"  class="form-control" value="" placeholder="JJ/MM/AAAA" onblur="verifNaissance(this)" required>
           </div>
         </div>
       </fieldset>
-    </div>
 
-
-      <div class="col-sm-7 divFormulaire " >
       <fieldset class="tir">
         <legend>Votre tir</legend>
         <div class="form-group">
-          <label class="col-md-2  col-md-offset-1 control-label" for="estGaucher">Gaucher </label>
-          <div >
+          <label class="col-sm-4 control-label" for="estGaucher">Gaucher </label>
+          <div class="col-sm-5">
             <input type="radio" name="estGaucher" id="estGaucher" value="1"> Oui
             <input type="radio" name="estGaucher" id="estGaucher" value="0" checked> Non
           </div>
@@ -108,8 +95,8 @@
         <!-- Les tirs -->
 
         <div class="form-group">
-          <label class="col-md-2 col-md-offset-1 control-label" for="doleGroupe">Dôle groupe </label>
-         <div >
+          <label class="col-sm-4 control-label" for="doleGroupe">Dôle groupe </label>
+          <div class="col-sm-5">
             <input type="radio" name="doleGroupe" id="doleGroupe" value="1" checked> Oui
             <input type="radio" name="doleGroupe" id="doleGroupe" value="0"> Non
           </div>
@@ -117,8 +104,8 @@
 
 
         <div class="form-group">
-          <label class="col-md-2  col-md-offset-1 control-label" for="duillier">Duillier </label>
-          <div>
+          <label class="col-sm-4 control-label" for="duillier">Duillier </label>
+          <div class="col-sm-5">
             <input type="radio" name="duillier" id="duillier" value="1" checked> Oui
             <input type="radio" name="duillier" id="duillier" value="0"> Non
           </div>
@@ -126,8 +113,8 @@
 
 
         <div class="form-group">
-          <label class="col-md-2  col-md-offset-1 control-label"for="montBlanc">Mont-blanc </label>
-          <div >
+          <label class="col-sm-4 control-label" for="montBlanc">Mont-blanc </label>
+          <div class="col-sm-5">
             <input type="radio" name="montBlanc" id="montBlanc" value="1" checked> Oui
             <input type="radio" name="montBlanc" id="montBlanc" value="0"> Non
           </div>
@@ -135,8 +122,8 @@
 
 
         <div class="form-group">
-          <label class="col-md-2  col-md-offset-1 control-label" for="perceNeige">Perce-neige </label>
-          <div>
+          <label class="col-sm-4 control-label" for="perceNeige">Perce-neige </label>
+          <div class="col-sm-5">
             <input type="radio" name="perceNeige" id="perceNeige" value="1" checked> Oui
             <input type="radio" name="perceNeige" id="perceNeige" value="0"> Non
           </div>
@@ -144,15 +131,15 @@
 
 
         <div class="form-group">
-          <label  class="col-sm-2 col-md-offset-1 control-label" for="club">Club</label>
-          <div >
-            <select name="listeClub">  
+          <label  class="col-sm-4 control-label" for="club">Club</label>
+          <div class="col-sm-5">
+            <select name="club">  
               <option value="">Pas de club choisi</option>  
               <?php
               $reponse = $bdd->query('SELECT * FROM club');
               while ($donnees = $reponse->fetch()){
                 ?>
-                <option value="<?php echo $donnees['idClub']; ?>"><?php echo $donnees['nom'] ?></option>
+                <option value="<?php echo $donnees['idClub']; ?>" name="club"><?php echo $donnees['nom'] ?></option>
                 <?php
               }
               ?>
@@ -162,8 +149,8 @@
 
 
         <div class="form-group">
-          <label  class="col-sm-2 col-md-offset-1 control-label" for="groupe">Groupe</label>
-          <div >
+          <label  class="col-sm-4 control-label" for="groupe">Groupe</label>
+          <div class="col-sm-5">
             <select class="" name="listeGroupe">
               <option value="pasDeGroupe">Pas de groupe choisi</option>
               <?php
@@ -179,87 +166,83 @@
         </div>
 
         <div class="form-group">
-          <label  class="col-sm-2 col-md-offset-1 control-label" for="nbEssai">Nombre d'essai</label>
-          <div >
-            <input type="number" step="1" value="0" min="0" max="5" for="nbEssai">
+          <label  class="col-sm-4 control-label" for="nbEssai">Nombre d'essai</label>
+          <div class="col-sm-5">
+            <input type="number" step="1" value="0" min="0" max="5" name="nbEssai">
           </div>
         </div>
 
         <div class="form-group">
-          <label  class="col-sm-2 col-md-offset-1 control-label" for="repas">Nombre de repas</label>
-          <div >
-            <input type="number" step="1" value="0" min="0" max="9" for="repas">
+          <label  class="col-sm-4 control-label" for="repas">Nombre de repas</label>
+          <div class="col-sm-5">
+            <input type="number" step="1" value="0" min="0" max="9" name="repas">
           </div>
         </div>
-      </fieldset></br>
-    </div>
-      <div class="col-sm-12 divFormulaire " >
+      </fieldset>
 
       <fieldset class="recap">
-        <legend class="text-center">Récapitulons</legend>
-
-        <div class="form-group ">
-          <label for="jour" class="col-sm-3 text-right control-label">Jour de tir</label>
-          <div class="col-sm-3">
-            <p class="form-control-static"><?php echo $_POST['jour']; ?></p>
-          </div>
-        </div>
+        <legend>Récapitulons</legend>
 
         <div class="form-group">
-          <label for="poste" class="col-sm-2  control-label">Votre poste</label>
+          <label for="jour" class="col-sm-4 control-label">Jour de tir</label>
           <div class="col-sm-4">
-            <p class="form-control-static"><?php echo $_POST['poste']; ?></p>
+            <input type="text" value=<?php echo $_POST['jour']; ?> name="jour" readonly="readonly">
           </div>
         </div>
 
         <div class="form-group">
-          <label for="debut" class="col-sm-3 text-right control-label">Heure de début</label>
-          <div class="col-sm-3">
-            <p class="form-control-static"><?php echo $_POST['heure']; ?></p>
-          </div>
-        </div>
-
-
-
-
-
-
-
-        <div class="form-group">
-          <label for="rangeur" class="col-sm-2 control-label">Nombre de rangeurs</label>
+          <label for="debut" class="col-sm-4 control-label">Heure de début</label>
           <div class="col-sm-4">
-            <p class="form-control-static"><?php echo "3"; ?></p>
+            <input type="text" value=<?php echo $_POST['heure']; ?> name="heure" readonly="readonly">
+          </div>
+        </div>
+
+
+        <div class="form-group">
+          <label for="poste" class="col-sm-4 control-label">Votre poste</label>
+          <div class="col-sm-4">
+          <!--  <p class="form-control-static"><?php //echo $_POST['poste']; ?></p> -->
+            <input type="text" value=<?php echo $_POST['poste']; ?> name="poste" readonly="readonly">
+          </div>
+        </div>
+
+
+        <div class="form-group">
+          <label for="prix" class="col-sm-4 control-label">Prix</label>
+          <div class="col-sm-4">
+            <input type="text" value="40" name="prix" readonly="readonly">
           </div>
         </div>
 
         <div class="form-group">
-          <label for="prix" class="col-sm-3 text-right control-label">Prix</label>
-          <div class="col-sm-3">
-            <p class="form-control-static"><?php echo "40 CHF"; ?></p>
+          <label for="rangeur" class="col-sm-4 control-label">Nombre de rangeurs</label>
+          <div class="col-sm-4">
+            <input type="text" value="3" name="rangeur" readonly="readonly">
           </div>
         </div>
 
         <div class="form-group">
-          <label for="coups" class="col-sm-2 control-label">Nombre de coups</label>
+          <label for="coups" class="col-sm-4 control-label">Nombre de coups</label>
           <div class="col-sm-4">
             <p class="form-control-static"><?php echo "35"; ?></p>
           </div>
         </div>
-        <div class="col-sm-12">
+
         <div class="form-group">
-          <label  class="col-sm-1 control-label" for="commentaire">Commentaire</label>
+          <label  class="col-sm-4 control-label" for="commentaire">Commentaire</label>
           <div class="col-sm-5">
             <textarea name="commentaire" rows="8" cols="80" class="form-control"></textarea>
           </div>
         </div>
       </fieldset>
 
-        <div class="col-sm-6">
+        <div class="col-sm-offset-4">
           <input type="submit" name="valider" class="btn btn-success btn-s" value="Valider">
+          <input type="hidden" name="valider">
           <a href="../planning" class="btn btn-warning">Annuler</a>
         </div>
 
-      </div>
+
 
       </div>
 
