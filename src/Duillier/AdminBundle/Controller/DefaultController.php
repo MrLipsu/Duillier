55a40247bb3928e8c76030e8039506cb0e83dd13
perrinyo <perrinyo.info@gmail.com>
2017-03-07 18:27:17 +0100

    maj yoan AdminBundle

--- src/Duillier/AdminBundle/Controller/DefaultController.php
+++ src/Duillier/AdminBundle/Controller/DefaultController.php
@@ -10,4 +10,35 @@ class DefaultController extends Controller
     {
         return $this->render('DuillierAdminBundle:Default:index.html.php');
     }
+
+    public function nouveauConcoursAction()
+    {
+        return $this->render('DuillierAdminBundle:Default:nouveauConcours.html.php');
+    }
+
+    public function gestionClubAction()
+    {
+        return $this->render('DuillierAdminBundle:Default:gestionClub.html.php');
+    }
+
+    public function modifierClubAction()
+    {
+        return $this->render('DuillierAdminBundle:Default:modifierClub.html.php');
+    }
+
+    public function supprimerClubAction()
+    {
+        return $this->render('DuillierAdminBundle:Default:supprimerClub.html.php');
+    }
+
+    public function ajouterClubAction()
+    {
+        return $this->render('DuillierAdminBundle:Default:ajouterClub.html.php');
+    }
+
+    public function gererRepasAction()
+    {
+        return $this->render('DuillierAdminBundle:Default:gererRepas.html.php');
+    }
+
 }
