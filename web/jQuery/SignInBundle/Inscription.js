5e4c70a5633ecff3ad6e78221c81f455f3dbd2f1
perrinyo <perrinyo.info@gmail.com>
2017-03-09 21:07:49 +0100

    maj web/jQuery/SignInBundle yoann

--- web/jQuery/SignInBundle/Inscription.js
+++ web/jQuery/SignInBundle/Inscription.js
@@ -1,17 +1,111 @@
 $(document).ready(function() {
   console.log("JQuery récupéré !");
 
-
-  $("#numberNbEssai").click(function() {
-    console.log("clique sur le nbEssai");
-
-
-  });
-
   init();
 
+  majRecapNombreDeCoups();
+  majPrix();
 
+
+  $("#nbEssais").change(function() {
+    majRecapNombreDeCoups();
+    majPrix();
+  });
+
+  $("#divTirs").change(function(){
+    majRecapNombreDeCoups();
+    majPrix();
+  });
+
+
+  $("#estGaucher").change(function(){
+    if ($("#poste").attr("value") != 2 && $("#poste").attr("value") != 4 && $("#poste").attr("value") != 6) {
+      $.notiModal.get("alerteGaucher").show();
+    }
+  });
+
+  creationDesNotif();
 });
+
+
+function creationDesNotif(){
+
+  $.notiModal.init("alerteGaucher", {
+    title:"Vous etes gaucher ? ",
+    content:"Si vous êtes gaucher, nous vous conseillons de vous inscrire sur les postes 2, 4 ou 6",
+    ok:"rediriger vers le planning",
+    max_width: 800,
+    sound: true,
+    force: true,
+
+    onOkClick:function(noti_modal){
+      window.location="../planning";
+    }
+  });
+}
+
+function majPrix(){
+
+  nbEssais = $("#nbEssais").val();
+  prixDesTirs = majPrixViaTirs();
+
+  var prix = nbEssais*2 +prixDesTirs;
+  $("#prix").attr('value', prix);
+  $("#prix").html(prix);
+}
+
+function majRecapNombreDeCoups(){
+  nbCoupsViaTirs = majNombreDeCoupsViaTirs();
+  nbCoupsViaEssais = $("#nbEssais").val()*5;
+  $("#recapNombreDecoups").html(nbCoupsViaTirs+nbCoupsViaEssais);
+  $("#recapNombreDecoups").attr("value", nbCoupsViaTirs+nbCoupsViaEssais);
+  majNombreDeRangeur();
+}
+
+function majNombreDeCoupsViaTirs(){
+  nombreDeCoupsViaTirs = 0;
+
+  if ($('#doleGroupe').is(':checked'))
+    nombreDeCoupsViaTirs = nombreDeCoupsViaTirs+10;
+  if ($("#duillier").is(':checked'))
+    nombreDeCoupsViaTirs = nombreDeCoupsViaTirs+6;
+  if ($("#montBlanc").is(':checked'))
+    nombreDeCoupsViaTirs = nombreDeCoupsViaTirs+9;
+  if ($("#perceNeige").is(':checked'))
+    nombreDeCoupsViaTirs = nombreDeCoupsViaTirs+6;
+
+  return nombreDeCoupsViaTirs;
+}
+
+function majPrixViaTirs(){
+  var prix = 6;
+
+  if ($('#doleGroupe').is(':checked'))
+    prix = prix +11;
+  if ($("#duillier").is(':checked'))
+    prix = prix +9.5;
+  if ($("#montBlanc").is(':checked'))
+    prix = prix +13;
+  if ($("#perceNeige").is(':checked'))
+    prix = prix +12.5;
+
+  return prix;
+}
+
+function majNombreDeRangeur(){
+  var nombreDeCoups = $("#recapNombreDecoups").attr("value");
+  var nombreDeRangeurs;
+  if (nombreDeCoups < 16) {
+    nombreDeRangeurs = 1
+  }else if (nombreDeCoups < 31){
+    nombreDeRangeurs = 2;
+  }else {
+    nombreDeRangeurs = 3;
+  }
+  $("#nombreDeRangeurs").html(nombreDeRangeurs);
+  $("#nombreDeRangeurs").attr("value",nombreDeRangeurs);
+}
+
 
 function init()
 {
@@ -30,13 +124,15 @@ function init()
 
         function(datas){
           $.each(datas, function(i, data){
-            console.log("mail : " + data.mail);
+            var date = data.anneeNaissance;
+            date = tranformDate(date);
+
             $("#idLicence").val(data.idLicence);
             $("#adresse").val(data.adresse);
             $("#ville").val(data.ville);
             $("#codePostal").val(data.codePostalTireur);
             $("#email").val(data.mail);
-            $("#anneeNaissance").val(data.anneeNaissance);
+            $("#anneeNaissance").val(date);
             //$("#email").val(data.anneeNaissance);
             //obligé de passer par une fonction en js natif(ne sais pas pourquoi Jquery n'y arrivais pas)
             setSelectValue("club",data.idClub);
@@ -53,21 +149,36 @@ function init()
 
 function setSelectValue(selectId, value)
 {
-	/*Récupération du select*/
-	var elmt = document.getElementById(selectId);
-	/*On parcourt les options du select*/
-	for (var i = 0; i < elmt.options.length; i++)
-	{
-		/*Si l'élément à la bonne valeur on le sélectionne*/
-		if(elmt.options[i].value == value)
-		{
-			elmt.selectedIndex = i;
-			return true;
-		}
-	}
-	/*On a pas trouvé la valeur on retourne faux*/
-	return false;
+    /*Récupération du select*/
+    var elmt = document.getElementById(selectId);
+    /*On parcourt les options du select*/
+    for (var i = 0; i < elmt.options.length; i++)
+    {
+        /*Si l'élément à la bonne valeur on le sélectionne*/
+        if(elmt.options[i].value == value)
+        {
+            elmt.selectedIndex = i;
+            return true;
+        }
+    }
+    /*On a pas trouvé la valeur on retourne faux*/
+    return false;
 }
+
+
+function tranformDate(date){
+  var regex = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+
+  if(!regex.test(date)){
+    var annee = date.substring(0,4);
+    var mois = date.substring(5,7);
+    var jour = date.substring(8,10);
+    date = jour + "/" + mois + "/" + annee;
+  }
+
+  return date;
+}
+
 
 function surligne(champ, erreur)
 {
